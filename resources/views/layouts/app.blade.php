edc231a7039bc0ee113e012420c5611f22245396
Luke Policinski <luke@lukepolo.com>
2019-01-26 08:41:06 -0500

    Making it work for a deployment

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=yes"
+    />
+    <title>Varie</title>
+    <link
+      href="https://fonts.googleapis.com/css?family=Montserrat:300"
+      rel="stylesheet"
+    />
+  <link href="/css/vendor-9ed4156beac1a4925818.css" rel="stylesheet"><link href="/css/app-47fbaedb9741c9ec5f28.css" rel="stylesheet"></head>
+  <body>
+    <noscript>
+      <strong
+        >We're sorry but Varie doesn't work properly without JavaScript enabled.
+        Please enable it to continue.</strong
+      >
+    </noscript>
+    <div id="app"></div>
+  <script type="text/javascript" src="/js/runtime~app-8e3ad3e26e3f28da8891.js"></script><script type="text/javascript" src="/js/vendor-c858916e5c1cb597912b.js"></script><script type="text/javascript" src="/js/app-612dcdc6d6e8bc6e3306.js"></script></body>
+</html>
