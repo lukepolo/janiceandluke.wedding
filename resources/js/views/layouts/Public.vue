c8bd4330f0628c7ce86f6785ecfc059b195092ed
Luke Policinski <luke.policinski@egov.com>
2019-01-28 12:42:48 -0500

    Fixing issue with prettier

--- resources/js/views/layouts/Public.vue
+++ resources/js/views/layouts/Public.vue
@@ -2,20 +2,12 @@
   <div id="app">
     <alerts></alerts>
     <nav class="nav">
-      <div class="nav--left">
-        Janice + Luke
-      </div>
+      <div class="nav--left">Janice + Luke</div>
       <div class="nav--right">
         <div class="nav--item"><span class="hide-small">Our</span> Wedding</div>
-        <div class="nav--item">
-          Travel
-        </div>
-        <div class="nav--item">
-          Registry
-        </div>
-        <div class="nav--item">
-          RSVP
-        </div>
+        <div class="nav--item">Travel</div>
+        <div class="nav--item">Registry</div>
+        <div class="nav--item">RSVP</div>
       </div>
     </nav>
     <router-view></router-view>
