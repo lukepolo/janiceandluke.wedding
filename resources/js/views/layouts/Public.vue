f85a55a28f168093633b919bf59b89e27c54a73d
Janice Falotico <jfalotic@gmail.com>
2019-01-28 11:08:08 -0500

    initial styles

--- resources/js/views/layouts/Public.vue
+++ resources/js/views/layouts/Public.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="app">
     <alerts></alerts>
+    <nav class="nav">
+      <div class="nav--left">
+        Janice + Luke
+      </div>
+      <div class="nav--right">
+        <div class="nav--item"><span class="hide-small">Our</span> Wedding</div>
+        <div class="nav--item">
+          Travel
+        </div>
+        <div class="nav--item">
+          Registry
+        </div>
+        <div class="nav--item">
+          RSVP
+        </div>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
