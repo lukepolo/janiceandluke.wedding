ceb52db709a25e6639f58ede3064108f625e3e13
Luke Policinski <luke.policinski@egov.com>
2019-02-26 20:31:40 -0500

    WIP

--- resources/js/views/rsvp/RSVP.vue
+++ resources/js/views/rsvp/RSVP.vue
@@ -14,26 +14,45 @@
         <p>
           Enter your name below:
         </p>
-        <form v-form="form" @submit.prevent="search">
+        <form v-form="searchForm" @submit.prevent="search">
           <div class="flyform--group">
             <input
+              id="lastName"
               name="search"
               type="text"
-              v-model="form.search"
+              v-model="searchForm.search"
               placeholder=" "
               validate
             />
-            <label for="branch">Last Name</label>
+            <label for="lastName">Last Name</label>
           </div>
 
           <div class="flyform--btns">
-            <button class="btn" :disabled="!form.isValid()">Submit</button>
+            <button class="btn" :disabled="!searchForm.isValid()">
+              Submit
+            </button>
           </div>
         </form>
       </div>
     </div>
-
-    <pre>{{ results }}</pre>
+    <form v-form="selectedGuestForm" @submit.prevent="selectGuest">
+      <template v-for="result in results">
+        <p>
+          <label>
+            <input
+              v-model="selectedGuestForm.guest"
+              name="guest"
+              type="radio"
+              :value="result.id"
+            />
+            {{ result.first_name }} {{ result.last_name }}
+          </label>
+        </p>
+      </template>
+      <button class="btn" :disabled="!selectedGuestForm.isValid()">
+        Submit
+      </button>
+    </form>
   </section>
 </template>
 
@@ -43,7 +62,10 @@ import Vue from "vue";
 export default Vue.extend({
   data() {
     return {
-      form: this.createForm({
+      selectedGuestForm: this.createForm({
+        guest: null,
+      }),
+      searchForm: this.createForm({
         search: null,
       }).validation({
         rules: {
@@ -54,7 +76,10 @@ export default Vue.extend({
   },
   methods: {
     search() {
-      this.$store.dispatch("guest/search", this.form.search);
+      this.$store.dispatch("guest/search", this.searchForm.search);
+    },
+    selectGuest() {
+      alert(`select guest ${this.selectedGuestForm.guest}`);
     },
   },
   computed: {
