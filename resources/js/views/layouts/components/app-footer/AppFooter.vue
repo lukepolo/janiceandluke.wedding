f5b37c13aca27f2ec4c20a8195a62e3f4b38b4c6
Janice Falotico <jfalotic@gmail.com>
2019-03-04 18:55:51 -0500

    adding footer text

--- resources/js/views/layouts/components/app-footer/AppFooter.vue
+++ resources/js/views/layouts/components/app-footer/AppFooter.vue
@@ -1,5 +1,17 @@
 <template>
   <footer class="footer">
+    <div class="footer--message">
+      <p>
+        Website made with <span class="text-error">&#10084;</span> by the future
+        Policinski's.
+      </p>
+      <p>
+        <strong
+          >He made it work. <span class="separator">|</span> She made it
+          pretty.</strong
+        >
+      </p>
+    </div>
     <div class="gradient">
       <div class="gradient-box gradient-box-lightest"></div>
       <div class="gradient-box gradient-box-light"></div>
