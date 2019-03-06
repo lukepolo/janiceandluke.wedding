2299411ae89ce07913c36e4af5b55b9003f174b8
Luke Policinski <luke@lukepolo.com>
2019-03-05 22:00:12 -0500

    Acutal data provided for seeder

---
+++ app/Imports/GuestImport.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Guest;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+
+class GuestImport implements ToCollection
+{
+    public function collection(Collection $rows)
+    {
+        foreach ($rows as $rowIndex => $row) {
+            if ($rowIndex < 1) {
+                continue;
+            }
+            // 1 = names
+            // 2 = adults
+            // 3 = allowed plus one
+
+            $guestNames = array_reverse(
+                array_map(
+                    'trim',
+                    preg_split('/&/', $row[1])
+                )
+            );
+
+            $lastName = '';
+            $connectedGuest = null;
+            foreach ($guestNames as $guestName) {
+                if (!empty($guestName) && $guestName !== 'Guest Count') {
+                    $nameParts = preg_split('/\s/', $guestName);
+                    $firstName = $nameParts[0];
+                    if (isset($nameParts[1])) {
+                        $lastName = $nameParts[1];
+                    }
+                    $connectedGuest = $this->createGuest($firstName, $lastName, !empty($row[3]), true, $connectedGuest);
+                }
+            }
+        }
+    }
+
+    private function createGuest($firstName, $lastName, $allowedGuest, $allowedRehearsalDinner, Guest $connectedGuest = null)
+    {
+        $guest = Guest::firstOrCreate([
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+        ]);
+
+        $guest->fill([
+            'allowed_guest' => $allowedGuest,
+            'allowed_rehearsal_dinner' => $allowedRehearsalDinner,
+            'guest_id' => empty($connectedGuest) ? null : $connectedGuest->id,
+        ]);
+
+        $guest->save();
+
+        if (!empty($connectedGuest)) {
+            $connectedGuest->update([
+                'guest_id' => $guest->id,
+            ]);
+        }
+
+        return $guest;
+    }
+}
--- database/migrations/2019_02_27_000152_create_guests_table.php
+++ database/migrations/2019_02_27_000152_create_guests_table.php
@@ -19,7 +19,6 @@ class CreateGuestsTable extends Migration
             $table->string('last_name');
 
             $table->integer('guest_id')->nullable();
-            $table->boolean('is_guest')->default(0);
             $table->boolean('allowed_guest')->default(0);
 
             $table->boolean('allowed_rehearsal_dinner')->default(0);
